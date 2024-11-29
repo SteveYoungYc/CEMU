2d22ac8f76c263ec8dee1f56164c9eb21951468d
SteveYoungYc <chenyan@nvidia.com>
2024-11-29 11:19:02 +0800

    add: more riscv cpu tests

--- tests/inst_test.sh
+++ tests/inst_test.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 
-# Define the directory containing the .bin files
-directory="$AMKERNEL_HOME/tests/cpu-tests/build"
-
-# Check if the directory exists
-if [ ! -d "$directory" ]; then
-    echo "Directory $directory does not exist."
-    exit 1
-fi
+# Define an array of directories containing the .bin files
+directories=("$AMKERNEL_HOME/tests/cpu-tests/build" 
+            "$EMU_HOME/riscv-tests-am/build" 
+            "$EMU_HOME/riscv-arch-test-am/build")
 
 # Initialize an array to hold failed test files
 failed_tests=()
 
-# Loop through all .bin files in the specified directory
-for file in "$directory"/*.bin; do
-    # Check if any .bin files exist
-    if [ -e "$file" ]; then
-        echo "Executing cemu with $file"
-        
-        # Run the cemu command and capture its output
-        output=$(./cemu "-b" "--img=$file")
-        
-        # Check if the output contains "CEMU Trap!"
-        if [[ "$output" == *"Good!"* ]]; then
-            echo "Test with $file: PASS"
-        else
-            echo -e "\e[31mTest with $file: FAIL\e[0m"
-            failed_tests+=("$file")  # Store the failed test file
-        fi
-    else
-        echo "No .bin files found in $directory."
-        break
+# Loop through each directory in the array
+for directory in "${directories[@]}"; do
+    # Check if the directory exists
+    if [ ! -d "$directory" ]; then
+        echo "Directory $directory does not exist."
+        continue  # Skip to the next directory if this one doesn't exist
     fi
+
+    # Loop through all .bin files in the current directory
+    for file in "$directory"/*.bin; do
+        # Check if any .bin files exist
+        if [ -e "$file" ]; then
+            echo "Executing cemu with $file"
+            
+            # Run the cemu command and capture its output
+            output=$(./cemu "-b" "--img=$file")
+            
+            # Check if the output contains "Good!"
+            if [[ "$output" == *"Good!"* ]]; then
+                echo -e "Test with $file: \033[0;32mPASS\033[0m"
+            else
+                echo -e "\e[31mTest with $file: FAIL\e[0m"
+                failed_tests+=("$file")  # Store the failed test file
+            fi
+        else
+            echo "No .bin files found in $directory."
+        fi
+    done
 done
 
 # Display failures in red at the end
